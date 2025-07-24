2a427f27db28d601b4f9c92153551390b656e4c4
KevinKaki14 <kevin.kakiyama14@gmail.com>
2025-07-24 15:56:58 -0500

    Update Cuenta.java

--- Proyecto/src/cSistema/aUsuario/Cuenta.java
+++ Proyecto/src/cSistema/aUsuario/Cuenta.java
@@ -1,16 +1,60 @@
 package cSistema.aUsuario;
 
-
-//informacion del UML
-//-String idCuenta
-//-String password
-//-Cliente cliente
-//+ activarCuenta()
-//+ cerrarCuenta()
-
-
-//AGREGAR PUNTOS 1 POR 10$ DE COMPRA
-
 public class Cuenta {
+  private String idCuenta;
+    private String password;
+    private Cliente cliente;
+    private boolean activa;
+    private int puntosAcumulados;
 
+    public Cuenta(String idCuenta, String password, Cliente cliente) {
+        this.idCuenta = idCuenta;
+        this.password = password;
+        this.cliente = cliente;
+        this.activa = false;
+        this.puntosAcumulados = 0;
+    }
+
+    public String getIdCuenta() {
+        return idCuenta;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public boolean isActiva() {
+        return activa;
+    }
+  
+    //Activar cuenta
+    public void activarCuenta() {
+        if (!activa) {
+            activa = true;
+            System.out.println("Cuenta activada exitosamente.");
+        } else {
+            System.out.println("La cuenta ya está activa.");
+        }
+    }
+
+    //Cerrar cuenta
+    public void cerrarCuenta() {
+        if (activa) {
+            activa = false;
+            System.out.println("Cuenta cerrada exitosamente.");
+        } else {
+            System.out.println("La cuenta ya está cerrada.");
+        }
+    }
+
+    //Pts por compra
+    public void agregarPuntos(double totalCompra) {
+        int puntos = (int)(totalCompra / 10);
+        puntosAcumulados += puntos;
+        System.out.println("Se agregaron " + puntos + " puntos a la cuenta.");
+    }
+
+    public int getPuntosAcumulados() {
+        return puntosAcumulados;
+    }
 }
